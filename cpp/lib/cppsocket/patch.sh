a6591f892f8f35f4ee4149a3cc001e9ba8e8bbfd
Klaus Wenger <kjwenger@yahoo.com>
2018-01-21 12:13:47 +0100

    changes for mac os x

--- cpp/lib/cppsocket/patch.sh
+++ cpp/lib/cppsocket/patch.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
+#set -x
 
-find ${1} -name '*.cpp' -exec sed -i 's/std::cout/std::cerr/g' {} \;
-
-# | \
-#xargs | \
-#echo
+case "$(uname -s)" in
+   Darwin)
+     find ${1} -name '*.cpp' -print | xargs sed -i '' 's/std::cout/std::cerr/g'
+     ;;
+   *)
+     find ${1} -name '*.cpp' -exec sed -i 's/std::cout/std::cerr/g' {} \;
+     ;;
+esac
